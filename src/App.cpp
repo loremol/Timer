da09efe13df6707b9d1707c32665cec5ac851f2f
loremol <cocco1805@gmail.com>
2023-06-17 16:45:37 +0200

    changed window size and set it to be non-resizable

--- src/App.cpp
+++ src/App.cpp
@@ -3,7 +3,9 @@
 
 bool App::OnInit() {
     auto *mainFrame = new MainFrame("Timer");
-    mainFrame->SetClientSize(700, 500);
+    mainFrame->SetClientSize(635, 375);
+    mainFrame->SetMinClientSize(wxSize(635,375));
+    mainFrame->SetMaxClientSize(wxSize(635,375));
     mainFrame->Center();
     mainFrame->Show();
     return true;
